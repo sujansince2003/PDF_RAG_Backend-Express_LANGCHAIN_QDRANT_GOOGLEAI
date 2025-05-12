9c29a621fcdded2909d87a034794ee948bc0150b
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-05-13 01:27:14 +0545

    backend init setup cors multer for handling file uploads

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import multer from "multer";
+import cors from "cors";
+const app = express();
+app.use(cors());
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, "-" + uniqueSuffix + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/upload/pdf", upload.single("pdf"), (req, res) => {
+  return res.json({ msg: "file uploaded" });
+});
+
+app.get("/", (req, res) => {
+  res.json({ msg: "hello this is sujan" });
+});
+
+app.listen(8000, () => {
+  console.log("Server is running on port 8000");
+});
