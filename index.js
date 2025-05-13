ceea0ed6829016ae77d49924882ad4aee269ecc3
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-05-13 20:34:14 +0545

    handle the user query and respond from the context

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import express from "express";
 import multer from "multer";
 import cors from "cors";
-const app = express();
-import { Queue } from "bullmq";
+import { Mistral } from "@mistralai/mistralai";
 
+import { Queue } from "bullmq";
+import { MistralAIEmbeddings } from "@langchain/mistralai";
+import { QdrantVectorStore } from "@langchain/qdrant";
+
+const client = new Mistral({ apiKey: "HS1StuaBt3APyl2O1MchRqUd5t7nIs3P" });
+
+const app = express();
 app.use(cors());
 
 const queue = new Queue("pdf-queue");
@@ -19,6 +25,46 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage: storage });
+
+app.get("/chat", async (req, res) => {
+  const userQuery = req.query.message;
+  const embeddings = new MistralAIEmbeddings({
+    model: "mistral-embed",
+    apiKey: "HS1StuaBt3APyl2O1MchRqUd5t7nIs3P",
+  });
+
+  const vectorStore = await QdrantVectorStore.fromExistingCollection(
+    embeddings,
+    {
+      url: "http://localhost:6333",
+      collectionName: "pdf_documents",
+    }
+  );
+
+  const ret = vectorStore.asRetriever({
+    k: 2,
+  });
+  const result = await ret.invoke(userQuery);
+
+  const SYSTEM_PROMPT = `
+  You are helfull AI Assistant who answeres the user query based on the available context from PDF File.
+  Context:
+  ${JSON.stringify(result)}
+  `;
+
+  const chatResult = await client.chat.complete({
+    model: "mistral-large-latest",
+    messages: [
+      { role: "system", content: SYSTEM_PROMPT },
+      { role: "user", content: userQuery },
+    ],
+  });
+
+  return res.json({
+    answer: chatResult.choices[0].message.content,
+    docs: result,
+  });
+});
 
 app.post("/upload/pdf", upload.single("pdf"), async (req, res) => {
   await queue.add(
